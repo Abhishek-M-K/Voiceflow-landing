78080a35187535a662b0ea6d5051b8cf7135bb57
Abhishek-M-K <khandareabhishek98@gmail.com>
2024-01-01 20:31:24 +0530

    feat : navbar completed (v1)

--- app/navbar/navigation-bar.tsx
+++ app/navbar/navigation-bar.tsx
@@ -1,9 +1,138 @@
-const NavigationBar = () => {
-  return (
-    <div>
-      <h4>Bar</h4>
-    </div>
-  );
-};
+"use client";
 
-export default NavigationBar;
+import * as React from "react";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+
+import Logo from "./logo";
+
+const precomponents: { title: string; description: string }[] = [
+  {
+    title: "Frozen",
+
+    description:
+      "The best AI-powered screen recorder you can get to record video lectures .",
+  },
+  {
+    title: "Sweep",
+
+    description:
+      "A disk-cleanup service which prevents your storage from being topped up with GenAI memory caching.",
+  },
+  {
+    title: "Tuscany",
+
+    description:
+      "Advanced template with more components and features. Use this template if you want to build a complex UI.",
+  },
+  {
+    title: "Amber",
+
+    description:
+      "Build landing page for your SaaS product or service within 20 minutes.",
+  },
+  {
+    title: "Gruffin",
+
+    description:
+      "Styled-component for building dashboard/analytic panels with smart AI-configuration.",
+  },
+  {
+    title: "Mantis",
+
+    description:
+      "A cloud service to store your ENV variables on cloud, protecting them from being expose.",
+  },
+];
+
+export function NavigationMenuBar() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+              <li className="row-span-3">
+                <NavigationMenuLink asChild>
+                  <a
+                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/"
+                  >
+                    <Logo />
+                    <div className="mb-2 mt-4 text-[16px] font-medium">
+                      VoiceflowAI Inc.
+                    </div>
+                  </a>
+                </NavigationMenuLink>
+              </li>
+              <ListItem title="Introduction">
+                Transform your business with our exclusive AI solutions.
+              </ListItem>
+              <ListItem title="Demo">
+                Get a beta demo of exclusive AI solutions with Voiceflow.
+              </ListItem>
+              <ListItem title="Community">
+                Join our hyperactive community for feedback and help.
+              </ListItem>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Solutions</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              {precomponents.map((component) => (
+                <ListItem key={component.title} title={component.title}>
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/docs" legacyBehavior passHref>
+            <NavigationMenuLink>Documentation</NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = "ListItem";
--- app/navbar/page.tsx
+++ app/navbar/page.tsx
@@ -1,12 +1,12 @@
 import ActionButtons from "./action-buttons";
 import Logo from "./logo";
-import NavigationBar from "./navigation-bar";
+import { NavigationMenuBar } from "./navigation-bar";
 
 const Navbar = () => {
   return (
     <div className=" flex justify-between items-center mt-1 px-8 border-b">
       <Logo />
-      <NavigationBar />
+      <NavigationMenuBar />
       <ActionButtons />
     </div>
   );
